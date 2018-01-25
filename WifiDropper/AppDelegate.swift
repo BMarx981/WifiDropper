249b2c2bfb46ed3b02c701945925fd55656b46e2
Brian Marx <b.marx981@gmail.com>
2018-01-25 17:53:15 -0500

    Commit to add the project

--- WifiDropper/AppDelegate.swift
+++ WifiDropper/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -41,6 +40,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        completionHandler(shouldPerformAction())
+    }
+
+    private func shouldPerformAction() -> Bool {
+        let url = URL(string: "App-Prefs:root=WIFI") //for WIFI setting app
+        if UIApplication.shared.canOpenURL(url!) {
+            let app = UIApplication.shared
+            app.open(url!)
+            return true
+        } else {
+            print("There was a problem")
+            return false
+        }
+    }
 
 }
 
--- WifiDropper/ViewController.swift
+++ WifiDropper/ViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import Foundation
+import SystemConfiguration.CaptiveNetwork
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var buttonName: UIButton!
+    let userDefaults = UserDefaults.standard
+    
+    @IBAction func didendEdit() {
+        userDefaults.set(textfield.text, forKey: "ip")
+        textfield.becomeFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        buttonName.layer.shadowOpacity = 0.7
+        buttonName.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        buttonName.setTitle(getWiFiSsid(), for: .normal)
+        if let ipNumber = userDefaults.string(forKey: "ip") {
+            let label = view.viewWithTag(123) as? UILabel
+            label?.text = ipNumber
+        }
     }
 
+    @IBAction func openSettings(_ sender: UIButton) {
+        //UIApplication.shared.open(URL(string:UIApplicationOpenSettingsURLString)!)
+        let sharedApp = UIApplication.shared
+        if let interfaces = CNCopySupportedInterfaces() as NSArray? {
+            for interface in interfaces {
+                if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as! CFString) as NSDictionary? {
+                    let auto = interfaceInfo[kCNNetworkInfoKeySSID as! String] as! NSString?
+                    
+                    print(auto)
+                }
+            }
+        }
+        let url = URL(string: "App-Prefs:root=WIFI") //for WIFI setting app
+        if sharedApp.canOpenURL(url!) {
+            if let theText = textfield.text {
+                let label = view.viewWithTag(123) as? UILabel
+                if theText == "" {
+                    if let ip = userDefaults.string(forKey: "ip") {
+                        label?.text = ip
+                    } else {
+                        label?.text = "0.0.0.0"
+                    }
+                } else {
+                    UIPasteboard.general.string = theText
+                    label?.text = theText
+                }
+            }
+            textfield.resignFirstResponder()
+            sharedApp.open(url!)
+        } else {
+            print("There was a problem")
+        }
+    }
+
+    func getWiFiSsid() -> String? {
+        var ssid: String?
+        if let interfaces = CNCopySupportedInterfaces() as NSArray? {
+            for interface in interfaces {
+                if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as! CFString)
+                    as NSDictionary? {
+                    
+                    ssid = interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
+                    break
+                }
+            }
+        }
+        return ssid
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
